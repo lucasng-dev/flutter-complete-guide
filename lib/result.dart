721727cda2bb3cacdd15028c576fc504b5b96588
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2021-08-17 21:42:34 -0300

    restart button

--- lib/result.dart
+++ lib/result.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
-  const Result(this.resultScore, {Key? key}) : super(key: key);
+  final VoidCallback resetHandler;
+  const Result(this.resultScore, this.resetHandler, {Key? key})
+      : super(key: key);
 
   String get resultPhrase {
     String resultText;
@@ -21,13 +23,21 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(
-          fontSize: 36,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text('Restart Quiz!'),
+            onPressed: resetHandler,
+          )
+        ],
       ),
     );
   }
